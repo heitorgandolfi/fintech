39d6f45297f00dd4a5c5a6245a20a7da1f1f8483
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-04-16 13:26:18 -0300

    feat: add useFetch hook

---
+++ src/hooks/useFetch/useFetch.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export function useFetch<T>(url: RequestInfo | URL, options?: RequestInit) {
+  const [data, setData] = React.useState<T | null>(null);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState<null | string>(null);
+
+  const optionsRef = React.useRef(options);
+  optionsRef.current = options;
+
+  React.useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const fetchData = async () => {
+      setLoading(true);
+      setData(null);
+
+      try {
+        const response = await fetch(url, {
+          signal,
+          ...optionsRef.current,
+        });
+        if (!response.ok) throw new Error(response.statusText);
+        const json = (await response.json()) as T;
+
+        if (!signal.aborted) setData(json);
+      } catch (error) {
+        if (!signal.aborted && error instanceof Error) setError(error.message);
+      } finally {
+        if (!signal.aborted) setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+}
