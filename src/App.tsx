6aa5d498478779d1532dd6cedfde9e88a63ced26
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-04-16 13:45:33 -0300

    refactor: routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import "./style.css";
-import { Sales } from "./pages/sales/Sales";
-import { SummaryPage } from "./pages/summary/Summary";
-
-import { Header } from "./components/header/Header";
-import { SideNav } from "./components/sideNav/SideNav";
+import { AppRoutes } from "./routes/routes";
 import { DataContextProvider } from "./context/dataContext/DataContext";
+import "./style.css";
 
 function App() {
   return (
     <DataContextProvider>
-      <div className="container">
-        <SideNav />
-        <main>
-          <Header />
-          <SummaryPage />
-          <Sales />
-        </main>
-      </div>
+      <AppRoutes />
     </DataContextProvider>
   );
 }
---
+++ src/routes/routes.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { SalePage } from "../pages/sale/Sale";
+import { SalesPage } from "../pages/sales/Sales";
+import { SummaryPage } from "../pages/summary/Summary";
+
+import { SideNav } from "../components/sideNav/SideNav";
+import { Header } from "../components/header/Header";
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <SideNav />
+        <main>
+          <Header />
+          <Routes>
+            <Route path="/" element={<SummaryPage />} />
+            <Route path="/sales" element={<SalesPage />} />
+            <Route path="/sales/:id" element={<SalePage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
